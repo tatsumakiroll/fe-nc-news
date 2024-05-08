5f0977b22a5661a97d443b015d104cf398155a60
tatsumakiroll <paul.shoreman@outlook.com>
2024-05-08 15:50:01 +0100

    adding loading to article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,16 +3,28 @@ import { getArticleById } from "../utils/utils";
 import { useParams } from "react-router";
 import CommentsList from "./CommentsList";
 import ArticleVoteCard from './ArticleVoteCard'
+import Lottie from "lottie-react";
+import loadingGraphic from "../assets/loadingGraphic.json";
 
 const Article = () => {
   const [currentArticle, setCurrentArticle] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
   const { article_id } = useParams();
 
   React.useEffect(() => {
     getArticleById(article_id).then((data) => {
       setCurrentArticle(data);
+      setLoading(false);
     });
   }, [currentArticle]);
+
+  if (loading) {
+    return (
+      <div>
+        <Lottie animationData={loadingGraphic} />
+      </div>
+    );
+  }
 
   return (
     <section>
